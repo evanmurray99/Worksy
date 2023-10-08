0476497540fa485bdd8967e587489f102439ae45
omeizan <omeizanoel@gmail.com>
2023-10-08 12:02:45 -0500

    Removed Keys file

--- Server/Config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    MONGO_URI:"mongodb+srv://worksy:WORKSY4350@cluster0.3gnygys.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp",
-    PORT:3001
-}
-
-MONGO_URI="mongodb+srv://worksy:WORKSY4350@cluster0.3gnygys.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
