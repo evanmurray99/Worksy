01fabb081126fdff1bf12fa58331a85719372fe3
Tung Xuan Nguyen <xtung1521@gmail.com>
2023-10-15 00:17:59 -0500

    add login functionality

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/login" element={<Login />}></Route>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</BrowserRouter>
 	);
--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,22 +1,90 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Login() {
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+	const navigate = useNavigate();
+
+	const logIn = (e) => {
+		e.preventDefault();
+
+		const url = 'http://localhost:3001/api/users/login';
+		axios
+			.post(url, { email: email, password: password })
+			.then((response) => {
+				if (response.status === 200) {
+					setTimeout(() => {
+						navigate('/'); // !change later
+					}, 1000);
+				}
+				// window.location.reload();
+				console.log('here');
+				console.log(response);
+			})
+			.catch((e) => {
+				console.log(e.message);
+			});
+		// fetch(url, {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 	},
+		// 	body: JSON.stringify({ email: email, password: password }),
+		// })
+		// 	.then((response) => {
+		// 		if (response.ok) {
+		// 			setTimeout(() => {
+		// 				navigate('/'); // !change later
+		// 			}, 1000);
+		// 		}
+		// 		console.log('here');
+		// 		console.log(response);
+		// 		return response.json();
+		// 	})
+		// 	.then((data) => {
+		// 		console.log(data);
+		// 	})
+		// 	.catch((e) => {
+		// 		console.log(e.message);
+		// 	});
+	};
 	return (
 		<>
 			<div className="bg-gray-100 h-screen w-full">
-				<form className="max-w-[400px] w-full mx-auto bg-white p-4">
+				<form
+					className="max-w-[400px] w-full mx-auto bg-white p-4"
+					onSubmit={logIn}
+				>
 					<div className="text-center py-6">
 						<h1 className="text-2xl font-bold text-center">LOGIN</h1>
 						<p>Welcome back. Sign into your account</p>
 					</div>
 					<div className="flex flex-col py-2">
-						<label htmlFor="">Email</label>
-						<input className="border p-2" type="email" />
+						<label htmlFor="email">Email</label>
+						<input
+							className="border p-2"
+							type="email"
+							id="email"
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+						/>
 					</div>
 
 					<div className="flex flex-col py-2">
-						<label htmlFor="">Password</label>
-						<input className="border p-2" type="password" />
+						<label htmlFor="password">Password</label>
+						<input
+							className="border p-2"
+							type="password"
+							id="password"
+							value={password}
+							onChange={(e) => {
+								setPassword(e.target.value);
+							}}
+						/>
 					</div>
 					<button className="border w-full my-5 py-2 bg-black hover:bg-gray-800 text-white">
 						Sign in
