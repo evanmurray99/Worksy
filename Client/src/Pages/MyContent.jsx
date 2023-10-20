4d20e195ba9f594ede81dd019d261724c2fb08a4
Tung Xuan Nguyen <xtung1521@gmail.com>
2023-10-20 02:25:23 -0500

    connect get services from frontend to backend

--- Client/src/Pages/MyContent.jsx
+++ Client/src/Pages/MyContent.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
-import img1 from '../Images/StubImg1.jpg';
-import img2 from '../Images/StubImg2.jpg';
+import { editService } from '../utils/Services';
 
 import { PostListView } from '../Components/PostListView';
 import { AccountForm } from '../Components/AccountForm';
@@ -10,7 +9,7 @@ import { NavBar } from '../Components/NavBar';
 import { Link } from 'react-router-dom';
 
 function postToElement(posts) {
-	let numPosts = posts.length;
+	let numPosts = posts.length; // don't have length
 	let postList = [];
 
 	for (var i = 0; i < numPosts; i++) {
@@ -23,6 +22,7 @@ function postToElement(posts) {
 export const MyContent = () => {
 	const [modalIsOpen, updateModalIsOpen] = useState(false);
 	const [user, setUser] = useState();
+	const [services, setServices] = useState();
 
 	useEffect(() => {
 		console.log('update content');
@@ -34,8 +34,18 @@ export const MyContent = () => {
 			.then((response) => response.json())
 			.then((data) => {
 				setUser(data.user);
-			});
+				console.log('done');
+			})
+			.then(() => {
+				getServices(user);
+			})
+			.catch((e) => console.log(e.message));
 	}, []);
+
+	// const setUserAndServices = (data) =>{
+	// 	setUser(data.user)
+
+	// }
 
 	const updateUser = (firstName, lastName, email, password) => {
 		console.log(firstName, lastName, email, password);
@@ -69,6 +79,27 @@ export const MyContent = () => {
 			.catch((e) => console.log(e.message));
 	};
 
+	const getServices = (user) => {
+		const url = 'http://localhost:3001/api/users/services/' + user._id;
+		fetch(url, {
+			method: 'GET',
+			headers: {
+				'Content-type': 'application/json',
+			},
+		})
+			.then((response) => {
+				if (response.status === 200) return response.json();
+				else throw new Error('Error in getService');
+			})
+			.then((data) => {
+				console.log('from getServices', data);
+				setServices(data);
+			})
+			.catch((e) => console.log(e.message));
+	};
+
+	let postList = services ? postToElement(services) : null;
+
 	const dynamicButtons = (
 		<React.Fragment>
 			<button className="leftAlign">
@@ -81,35 +112,6 @@ export const MyContent = () => {
 			</button>
 		</React.Fragment>
 	);
-
-	//This needs to be replaced by a api call to get all posts created by this user.
-	//There may need to be some additional parsing as I am assuming the list is a list of post objects.
-	let accordContent = [
-		{
-			id: 0,
-			user: 'RandomUser',
-			price: '$45.49',
-			title: 'Test Post',
-			description:
-				'Simple description designed to test how well the view post handles longer text inputs.',
-			pinImg: img1,
-			categories: ['test', 'tags'],
-		},
-		{
-			id: 1,
-			user: 'TestUser',
-			price: '$175.00',
-			title: 'Another Post',
-			description: 'Test descript',
-			pinImg: img2,
-			categories: ['diffTag', 'tag2', 'testTag'],
-		},
-	];
-
-	let postList = postToElement(accordContent);
-
-	//this needs to be replaced by a getUser request. The current assumption is that getUser returns an object.
-	//There may need to be some parsing if the return value is not an object.
 
 	return (
 		<React.Fragment>
