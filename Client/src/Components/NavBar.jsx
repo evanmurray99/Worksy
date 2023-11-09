758239b201f2a6f39409e7ce2caaee8a8f4ed7d1
chayward3113 <haywardc@myumanitoba.ca>
2023-11-09 15:25:26 -0600

    I finished setting up the logout process.

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import PopUpModal from './PopUpModal'
 import CreatePostForm from './CreatePostForm'
+import Cookies from 'js-cookie'
 import '../Styles/NavBar.css'
+
+function logout()
+{
+    Cookies.remove("token");
+}
 
 export default function NavBar ({leftButtons, modalIsOpen, updateModalIsOpen}){
     return (
@@ -10,7 +16,7 @@ export default function NavBar ({leftButtons, modalIsOpen, updateModalIsOpen}){
             <nav>
                 <Link id="worksy" className="navLinks leftAlign" to="/home">Worksy</Link>
                 {leftButtons}
-                <button className="rightAlign"><Link className="navLinks" to="/login">Logout</Link></button>
+                <button className="rightAlign"><Link className="navLinks" onClick={logout} to="/login">Logout</Link></button>
             </nav>
             
             <PopUpModal title="Create new post" isOpen={modalIsOpen} updateIsOpen={updateModalIsOpen} content={<CreatePostForm/>}/>
