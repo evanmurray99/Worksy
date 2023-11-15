f3e9c1320570e6c001357f553bd1397c5ea46c7a
Evan <howlndog@hotmail.ca>
2023-11-15 16:10:32 -0600

    bug fixes for imports and search

--- Client/src/Components/SearchBar.jsx
+++ Client/src/Components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'React'
+import {useState, useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
 import '../Styles/NavBar.css'
 export default function SearchBar({}){
--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from '../components/Navbar';
+import NavBar from '../Components/NavBar';
 import {Link} from 'react-router-dom'
 
 export default function Home() {
